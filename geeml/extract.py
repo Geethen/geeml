2b9b3dcd335916bef2a407a60c71332d11fc077d
Geethen <geethen.singh@gmail.com>
2022-06-29 15:27:10 +0200

    updated extracted class to use geedim

--- geeml/extract.py
+++ geeml/extract.py
@@ -11,9 +11,10 @@ import csv
 import sys
 
 import retry
+
 class extractor:
 
-    def __init__(self, covariates, grid, aoi, scale, dd, workers, target = None, spcvGridSize = None):
+    def __init__(self, covariates, aoi, scale, dd, target = None, spcvGridSize = None):
         """
         Prepares explanatoy variables and response variables for data exatraction.
         A grid for spatial cross validation (spcv) is also added as a band.
@@ -23,20 +24,17 @@ class extractor:
             covariates (ee.List, ee.Image, ee.ImageCollection): List should contain images (ee.Image). If imagecollection,
                 each image should be a single covariate.
 
-            grid (ee.Image or ee.FeatureCollection): A grid created using the createGrid function since the unique patch
-                id's need to match grid values.
-
             aoi (ee.Feature): The area of interest that designates the study area.
 
             scale (int): The spatial resolution for exported data.
 
             dd (string): The destination to save exported/downloaded data.
 
-            target (ee.Image): The response variable. Optionally, this can be included in the covariates list.
+            target (ee.FeatureCollection, ee.Image): The response variable. Optionally, this can be included in the covariates list.
+                If ee.FeatureCollection, covariates will be sampled at all target pixels.
 
             spcvGridSize (int): The size of blocks to use for spcv. All samples within a block are 
                 assigned the same unique id.
-            
         
         Returns:
             ee objects required for data extraction.
@@ -45,33 +43,26 @@ class extractor:
                 if multiband image or multiple covariates.
             
             target (ee.Image): The target
-
-            grid (ee.Image or ee.FeatureCollection): used as workers during data extraction
-
-            aoi (ee.Feature): The area of interest
-
-            scale (int): The scale (GSD) of the output (covariate and/or target) in metres.
+            
         """
         self.covariates = covariates
-        self.grid = grid
         self.aoi = aoi
         self.scale =scale
         self.dd = dd
         self.target = target
         self.spcvGridSize = spcvGridSize
-        self.workers = workers
         
+        # grid for spcv
         if self.spcvGridSize is not None:
             #Generate Grid for spcv
-            self.spcvGrid, _ = createGrid(self.spcvGridSize, aoi = self.aoi, vect = False)
+            self.spcvGrid, self.ids = createGrid(self.spcvGridSize, aoi = self.aoi, vect = False)
 
             #Compile covariates
             if self.covariates.name() in ['Image','ImageCollection']:
-                finalCovariates = self.spcvGrid.addBands(ee.ImageCollection(self.covariates).toBands()).float()
+                bandnames = self.covariates.bandNames().getInfo()
+                finalCovariates = self.spcvGrid.addBands(ee.ImageCollection(self.covariates).toBands()).float().rename(['id'] + bandnames)
             elif self.covariates.name() == 'List':
-                for covariate in self.covariates:
-                    self.spcvGrid = self.spcvGrid.addBands(ee.Image(covariate))
-                finalCovariates = self.spcvGrid.float()
+                finalCovariates = self.spcvGrid.addBands(ee.ImageCollection.fromImages(self.covariates).toBands().float())
             else:
                 raise TypeError("Only takes single image (ee.Image), list of image id's or ee.Imagecollection as covariates")
         else:
@@ -79,198 +70,100 @@ class extractor:
             if self.covariates.name() in ['Image','ImageCollection']:
                 finalCovariates = ee.ImageCollection(self.covariates).toBands().float()
             elif self.covariates.name() == 'List':
-                for self.covariate in covariates:
-                    finalCovariates = ee.Image(covariate).float()
+                finalCovariates = ee.ImageCollection.fromImages(self.covariates).toBands().float()
             else:
                 raise TypeError("Only takes single image (ee.Image), list of image id's or ee.Imagecollection as covariates")
+        
+        # single or multi band image
         self.covariates = finalCovariates
 
         #Accomodate target
-        if self.target is not None:
-            self.target = self.target.rename('target').float()
+        if self.target is not None and self.target.name() == 'ee.Image':
+            self.target = self.target.rename('target').float().clip(self.aoi)
 
-    def extractAoi(self, index, fid):
+    def extractAoi(self):
 
         """
         Extract image patches of covariates within an aoi(specified in prepareForExtraction).
-        
-        Args:
-            index (int): index returned by enumerate
-            fid (int): unique id of grid. Each grid cell corresponds to a worker.
-            
+                    
         Returns:
-            Data (image patches as GeoTiff) exported to download directory (dd) specified in prepareForExtraction function.
+            Default: Data (image patches as GeoTiff) exported to download directory (dd).
+            
         """
-        assert ('linux' in sys.platform), "This code runs on Linux only."
-        # Select patch
-        if self.grid.name() == 'Image':
-            patchSize = self.grid.projection().nominalScale()
-            cell = self.grid.updateMask(self.grid.eq(fid).selfMask())
-            vCell = cell.addBands(cell).reduceToVectors(ee.Reducer.mean(), self.aoi, patchSize, maxPixels=1e13)
-        elif self.grid.name() == 'FeatureCollection':
-            vCell = ee.Feature(self.grid.filter(ee.Filter.eq('id', fid)).first())
-      
+        
         # Download patch as tif ()
+        if not os.path.exists(self.dd + '/X/'):
+                os.makedirs(self.dd + '/X/')
+        os.chdir(self.dd + '/X/')
+        geemap.download_ee_image(self.covariates, crs= 'EPSG:4326', filename= os.path.join(self.dd, f"X/X.tif"),\
+                                scale = self.scale, region= self.aoi.geometry())
+
         if self.target is not None:
             #Set directory
-            if os.path.exists(self.dd):
-                os.chdir(self.dd+'X/')
-            else:
-                os.makedirs(self.dd+'X/')
-            geemap.ee_export_image(self.covariates, crs= 'EPSG:4326', filename= os.path.join(dd, f"X_{fid}.tif"),\
-                                scale = self.scale, region= ee.Feature(vCell.first()).geometry(), file_per_band=False)
+            if not os.path.exists(self.dd + '/Y/'):
+                os.makedirs(self.dd + '/Y/')
+            os.chdir(self.dd + '/Y/')
+            # Download patch as tif
+            geemap.download_ee_image(self.target, crs= 'EPSG:4326', filename= os.path.join(self.dd, f"Y/Y.tif"),\
+                                scale = self.scale, region= self.aoi.geometry())
 
-            #Set directory
-            if os.path.exists(self.dd):
-                os.chdir(self.dd+'Y/')
-            else:
-                os.makedirs(self.dd+'Y/')
-            # Download patch as tif ()
-            geemap.ee_export_image(self.target, crs= 'EPSG:4326', filename= os.path.join(dd, f"Y_{fid}.tif"),\
-                                scale = self.scale, region= ee.Feature(vCell.first()).geometry(), file_per_band=False)
-        else:
-            geemap.ee_export_image(self.covariates, crs= 'EPSG:4326', filename= os.path.join(dd, f"X_{fid}.tif"),\
-                                scale = self.scale, region= ee.Feature(vCell.first()).geometry(), file_per_band=False)
-    #extract data at points
-    def extractPoints(index, fid):
+        
+    def extractPoints(self, batchSize, prefix = 'task_'):
         """
-        Extract covariate data at points.
+        Extract covariate data at points when points is set to true in extractor.
         
-        Args:
-            index (int): index returned by enumerate
-            fid (int): unique id of grid. Each grid cell corresponds to a worker.
-            
-        Returns:
-            Data (csv) exported to download directory (dd) specified in prepareForExraction function.
+       Args:
+           batchSize (int): The number of batches to split job into
+           prefix (str): Defaults to 'task_'. Used as file names.
+       Returns:
+           If points = True, data (csv) exported to download directory (dd).
         """
-        assert ('linux' in sys.platform), "This code runs on Linux only."
-        # Select patch
-        if grid.name() == 'Image':
-            patchSize = grid.projection().nominalScale()
-            cell = grid.updateMask(grid.eq(fid).selfMask())
-            vCell = cell.addBands(cell).reduceToVectors(ee.Reducer.mean(), aoi, patchSize)
-        elif grid.name() == 'FeatureCollection':
-            vCell = ee.Feature(grid.filter(ee.Filter.eq('id', fid)).first())
+        #Set working directory
+        if not os.path.exists(self.dd):
+                os.makedirs(self.dd)
+        os.chdir(self.dd)
         
-        #Filter points to patch
-        locations = points.filterBounds(vCell)
-        
-        #Extract data
-        size = locations.size().getInfo()
-        if  size == 0:
-            print(f'Skipping block {index} with zero features')
+        # Sample all pixels at points
+        if self.target.name() == 'Image':
+            # Extract data
+            points = self.target.sample(**{'dropNulls': True, 'factor': None,\
+                                'numPixels': None,  'region': self.aoi,\
+                            'scale': self.scale,'projection': projection,'geometries':True})
         else:
-            print(f'Extracting block {index} data for {size} gedi point(s)')
-            data = covariates.reduceRegions(locations, ee.Reducer.first(), scale)
-            properties = covariates.propertyNames()
+            points = self.target.filterBounds(self.aoi)
+        
+        size = points.size().getInfo()
+        pointsList = points.toList(size)
+        
+        batchSize = batchSize
+        
+        for i, batch in enumerate(range(0, size, batchSize)):
+            fc = ee.FeatureCollection(pointsList.slice(i, i+batchSize))
+            task_no = str(i+1)
+            print(f'Extracting data for task {i+1} with {fc.size().getInfo()} point(s)')
+            tsk_name = prefix + task_no
+
+            data = self.covariates.reduceRegions(fc, ee.Reducer.first(), self.scale)
+            properties = data.first().propertyNames()
 
             output = data.map(lambda ft: ft.set('output', properties.map(lambda prop: ft.get(prop))))
             result = output.aggregate_array('output').getInfo()
 
             # Write the results to a file.
-            filename = f'block_{fid}_results_{index}.csv'
+            filename = f'{tsk_name}.csv'
             with open(filename, 'w') as f:
                 writer = csv.writer(f)
                 # write the header
                 writer.writerow(properties.getInfo())
                 # write multiple rows
                 writer.writerows(result)
-
-    #Extract data around points
-    def extractSparsePatches(index, fid):
+            
+    def extractRegions(self, reduce = True):
         """
-        Extract summary statistics for neighbourhoods around points.
+        Extract summary statistics of covariates for regions.
         
-        Args:
-            index (int): index returned by enumerate
-            fid (int): unique id of grid. Each grid cell corresponds to a worker.
             
         Returns:
-            Data (GeoTif) exported to download directory (dd) specified in prepareForExraction function.
-            Band 1 cooresponds to the spcvGrid and the last band corresponds to the target variable.
+            Data (csv) exported to download directory (dd).
             
         """
-        assert ('linux' in sys.platform), "This code runs on Linux only."
-        # Select patch
-        if grid.name() == 'Image':
-            #patchSize = grid.projection().nominalScale()
-            cell = grid.updateMask(grid.eq(fid).selfMask())
-            vCell = cell.addBands(cell).reduceToVectors(**{'reducer':ee.Reducer.first()\
-                                    , 'geometry':aoi, 'scale': scale, 'maxPixels':1e13})
-        elif grid.name() == 'FeatureCollection':
-            vCell = ee.Feature(grid.filter(ee.Filter.eq('id', fid)).first())
-        
-            target = finalCovariates.select('target').clip(vCell)
-            vTarget = target.int().addBands(target).reduceToVectors(ee.Reducer.last(), vCell, 25)#fix scale value
-            size = vTarget.size().getInfo()
-        
-        if size==0:
-            print(f'Skipping patch {fid} with zero features')
-        else:
-            print(f'Extracting patch {fid} with {size} features')
-        # Download patch as tif ()
-        geemap.ee_export_image(finalCovariates, filename= os.path.join(dd, f"covs_{fid}.tif"),\
-                crs= 'EPSG:4326', scale= scale, region= vTarget.geometry(), file_per_band=False)
-
-    #extract data at points
-    def extractPointsNeighbourhood(index, fid):
-        """
-        Extract covariate data at points.
-        
-        Args:
-            index (int): index returned by enumerate
-            fid (int): unique id of grid. Each grid cell corresponds to a worker.
-            
-        Returns:
-            Data (csv) exported to download directory (dd) specified in prepareForExraction function.
-        """
-        assert ('linux' in sys.platform), "This code runs on Linux only."
-        # Select patch
-        if grid.name() == 'Image':
-            patchSize = grid.projection().nominalScale()
-            cell = grid.updateMask(grid.eq(fid).selfMask())
-            vCell = cell.addBands(cell).reduceToVectors(**{'reducer':ee.Reducer.first()\
-                                    , 'geometry':aoi, 'scale': patchSize, 'maxPixels':1e13})
-        elif grid.name() == 'FeatureCollection':
-            vCell = ee.Feature(grid.filter(ee.Filter.eq('id', fid)).first())
-        
-        #Create neighbourhood around points
-        target = finalCovariates.select('target')
-        points = target.sample(region = vCell, scale = patchSize)\
-                .map(lambda x: x.buffer(12.5).bounds())
-        
-        #Extract data
-        size = points.size().getInfo()
-        if  size == 0:
-            print(f'Skipping block {index} with zero features')
-        else:
-            print(f'Extracting block {index} data with {size} features')
-            # Download patch as tif ()
-            geemap.ee_export_image(finalCovariates.updateMask(target), filename= os.path.join(dd, f"covs_{fid}.tif"),\
-                crs= 'EPSG:4326', scale= scale, region= vCell.geometry(), file_per_band=False)
-
-# @retry(tries=10, delay=1, backoff=2)
-def pExtract(extractFunction, workers, nProcesses = 25):
-    """
-    Extract data from gee using parrallel processing
-    
-    Args:
-        extractFunction : One of extractAoi, extractPoints, extractSparsePoints or extractPointNeighbourhood
-        
-        nProcesses (int): The Number of parrallel processes
-        
-        workers (list): A list of unique ids corresponding to unique grid id's
-        
-    Returns:
-        Downloads files (csv or Geotiff) to Google Drive
-    """
-    
-    if __name__ == '__main__':
-        assert ('linux' in sys.platform), "This code runs on Linux only."
-        logging.basicConfig()
-
-        pool = multiprocessing.Pool(nProcesses)
-        pool.starmap(extractFunction, enumerate(workers))
-
-        pool.close()
-        pool.join()
